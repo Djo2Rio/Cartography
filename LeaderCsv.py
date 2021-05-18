a1c2e11f57c900350ebd7f5fca59098b8b427c02
Jevts2 <jeremy.rochias@epita.fr>
2021-05-18 17:26:51 +0200

    [IN PROGRESS] fichier load json, csv, xlsx

---
+++ LeaderCsv.py
@@ -0,0 +1,18 @@
+import csv
+
+
+def get_csv_data(csv_file):
+    # Colonnes: Projet, Binôme, Contact Leader, Contact Binôme, Téléphone Leader, Téléphone Binôme
+    data = []
+    with open(csv_file, "r") as file:
+        for row in file:
+            data.append(row.encode("latin-1").decode("utf-8").strip("\ufeff")[:-1].split(";"))
+    return data
+
+def main():
+    var = get_csv_data("Listeleads.csv")
+    for n in var : 
+        print(n)
+
+    
+main()
