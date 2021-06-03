a5fc8cee5a438fdbde3c2337489f66564f1ab4b0
Joseph Deriaux <joseph.deriaux@epita.fr>
2021-06-03 11:29:10 +0200

    [WIP]: Added Maya, Michel, Mai-Linh, Caroline De Paoli and the overall project + pole class for future ameliorations
    Signed-off-by: Joseph Deriaux <joseph.deriaux@epita.fr>

--- main.py
+++ main.py
@@ -11,9 +11,14 @@ class Partenaire(StructuredNode):
     nom = StringProperty(required = True, unique_index=True,)
     chefs = RelationshipFrom('Etudiant', 'échange avec')
     teams = RelationshipFrom('Teams', 'travaille pour')
+    projet = Relationship('Projet', 'Est un sous projet de')
+
+class Projet(StructuredNode):
+    nom = StringProperty(required = True, unique_index=True,)
 
 class Personne(StructuredNode):
-    nom = StringProperty(required = True, unique_index=True,)
+    nom = StringProperty(required = True, unique_index=True)
+    pole = Relationship('Pole', 'appartienne à')
 
 class Etudiant(Personne):
     école = StringProperty()
@@ -21,6 +26,22 @@ class Etudiant(Personne):
 class ChefDeProjet(Etudiant):
     partenaire = RelationshipTo('Partenaire', 'échange avec')
     binome = Relationship('Etudiant', 'collabore avec')
+
+class Leader(Personne):
+    partenaire = RelationshipTo('Projet', 'définit les objectifs de')
+
+class Leads(Personne):
+    leader = Relationship("Leader", "échange des informations avec")
+    partenaire = RelationshipTo('Projet', 'pilote et gère')
+    binome = Relationship('Leads', 'collabore avec')
+    Consultant = Relationship('Consultant', 'consulte')
+
+
+class Consultant(Personne):
+    pole = Relationship('Pole', 'aide')
+
+class Pole():
+    nom = StringProperty(required = True, unique_index=True)
 
 class Eleves(Etudiant):
     # projet
@@ -38,8 +59,9 @@ class Sherpa(Personne):
     # lien evrs sherpa
     sherpa2 = Relationship('Sherpa', 'travaille avec')
 
+
 # Utility Fonction
-def addleadsAndPartenaire(leads):
+def addleadsAndPartenaire(leads, projet):
     for row in leads:
         if (row[0] != "Projet"):
             try:
@@ -56,12 +78,13 @@ def addleadsAndPartenaire(leads):
                 partenaire = Partenaire(nom=row[0]).save()
             chef.partenaire.connect(partenaire)
             sousfifre.binome.connect(chef)
+            partenaire.projet.connect(projet)
             #sousfifre.partenaire.connect(partenaire)
 
 def addIsgTeams(teams):
     for index, row in teams.iterrows():
             #print(row)
-            print("nom: ", row[1] +" " + row[2], "equipe: ", row[4], "partenaire: ", row[5], "sherpa1", row[7], "sherpa2", row[8])
+            #print("nom: ", row[1] +" " + row[2], "equipe: ", row[4], "partenaire: ", row[5], "sherpa1", row[7], "sherpa2", row[8])
             try:
                 teams = Teams.nodes.get(numéro=row[4])
             except DoesNotExist:
@@ -89,9 +112,20 @@ def addIsgTeams(teams):
 
 # Main Function
 def main():
+    projet = Projet(nom="Planète Solidaire").save()
+    leader = Leader(nom="Caroline DePaoli").save()
+    leader.partenaire.connect(projet)
+
+    michel = Consultant(nom="Michel Sasson").save()
+
+    mai= Leads(nom="Mai-Linh Lannes").save()
+    maya = Leads(nom="Maya Hannachi").save()
+    mai.partenaire.connect(projet)
+    mai.binome.connect(maya)
+    mai.Consultant.connect(michel)
     leads = get_csv_data("Listeleads.csv")
     var = get_xls_data("EFFECTIFS_CAMPUS.xlsx")
-    addleadsAndPartenaire(leads)
+    addleadsAndPartenaire(leads, projet)
     addIsgTeams(var)
 
 
